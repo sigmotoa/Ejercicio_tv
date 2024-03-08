14543fe36367a871dc8a65deac122a74369451f8
sigmotoa <si.galvismotoa@gmail.com>
2024-03-08 07:16:32 -0500

    Creando un televisor

---
+++ src/main/java/Televisor.java
@@ -0,0 +1,16 @@
+public class Televisor {
+
+    String marca;
+    int taman_o;
+    boolean encendido;
+    byte volumen;
+    byte canal;
+    String [] entrdas;
+
+
+    public void setEncendido(boolean encendido) {
+        this.encendido = !encendido;
+    }
+
+
+}
